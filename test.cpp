8c1dff96057257be563826ec02e2887cd0be1c2d
Mr_Banois <peeterluttrell@gmail.com>
2026-06-25 16:37:09 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     int temp;
-    std::cout << "hello world version 1.0";
+    std::cout << "hello world version 2.0";
     std::cin.clear();
     std::cin >> temp;
     std::cout << temp;
