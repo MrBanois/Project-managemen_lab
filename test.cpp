edbf1710c19a964b969989458c778ff7d3ce4cec
Mr_Banois <peeterluttrell@gmail.com>
2026-06-25 16:34:44 +0700

    commit1

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int temp;
+    std::cout << "hello world version 1.0";
+    std::cin.clear();
+    std::cin >> temp;
+    std::cout << temp;
+    //_sleep(1000);
+}
